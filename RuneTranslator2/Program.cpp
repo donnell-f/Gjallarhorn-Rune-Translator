10e5f3c75a380eff640253713b600098a74c7aa2
Regexxx <Regexxx@users.github.com>
2018-06-16 07:29:31 -0500

    Added actual code.
    (Title)

---
+++ RuneTranslator2/Program.cpp
@@ -0,0 +1,13 @@
+#include "GForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+int main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	RuneTranslator2::GForm form;
+	Application::Run(%form);
+}
